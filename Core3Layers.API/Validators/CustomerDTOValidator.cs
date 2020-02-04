69acf83348b956b64a97a01cbd0c8869f04ecb26
LUCAS RODRIGUES SENA <msbowelite@gmail.com>
2020-02-04 11:57:03 -0300

    Validation

--- Core3Layers.API/Validators/CustomerDTOValidator.cs
+++ Core3Layers.API/Validators/CustomerDTOValidator.cs
@@ -7,7 +7,9 @@ namespace Core3Layers.API.Validators
     {
         public CustomerDTOValidator()
         {
-            RuleFor(x => x.Name).NotNull().Length(11);
+            RuleFor(x => x.Name).NotNull().MaximumLength(100);
+            RuleFor(x => x.Email).NotNull().MaximumLength(100);
+            RuleFor(x => x.PostalCode).Length(7).When(w => w.PostalCode != null);
         }
     }
 }
